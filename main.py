598a5aa02eaa8e552d64837be2ea0036127f7700
Veronika-c <veronikachornii2@gmail.com>
2026-02-19 10:25:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from student import Student
 s1 =Student()
 
 #read data
-pass_l, pass_a, pass_b, combo = pathways.get_scores()
-print(pass_l, pass_a, pass_b, combo)
+pass_l, pass_a, pass_b, combo, basic_l, basic_a, basic_b = pathways.get_scores()
 
 def on_submit():
     # 1. Get the text from the entry field
@@ -74,18 +73,34 @@ def create_info_screen(container):
     def on_submit():
         lit_score = lit_entry.get()
         s1.lit_score = int(lit_score)
-        print(s1.lit_score)
         bio_score = bio_entry.get()
         s1.bio_score = int(bio_score)
-        print(s1.bio_score)
         al_score = al_entry.get()
         s1.al_score = int(al_score)
-        print(s1.al_score)
-        s1.grade = 11
+        grade = grade_entry.get()
+        s1.grade = int(grade)
+        cte = check_cte.get()
+        cert = check_cert.get()
+        if cte:
+            s1.cte = True
+        if cert:
+            s1.cert = True
+        
+        
         
         s1.result1 = pathways.pathway1_result(s1.lit_score, s1.al_score, s1.bio_score, s1.grade, pass_l, pass_a, pass_b)
         global path1_screen
         path1_screen = create_path1_screen(main_container)
+
+        #This is calling pathway 2 and setting the result
+        s1.result2 = pathways.pathway2_result(s1.lit_score, s1.al_score, s1.bio_score, s1.grade, pass_l, pass_a, pass_b, combo,  basic_l, basic_a, basic_b )
+        global path2_screen
+        path2_screen = create_path2_screen(main_container)
+
+        #This is calling pathway 3 and setting the result
+        s1.result3 = pathways.pathway3_result( s1.cte , s1.cert, s1.grade , )
+        global path3_screen
+        path3_screen = create_path3_screen(main_container)        
         
     frame = tk.Frame(container, bg="lightgrey")
     frame.grid(row=0, column=0, sticky="nsew")
@@ -95,6 +110,13 @@ def create_info_screen(container):
 
     info_title = tk.Label(frame, text="Student Information", font=("Times New Roman", 18), bg="lightgrey")
     info_title.pack(anchor='w', padx=10, pady=5)
+
+
+    #grade
+    grade_entry = tk.Entry(frame)
+    grade_label = tk.Label(frame, text=" Enter your grade as a number: ", font = ("Times New Roman", 9))
+    grade_label.pack(anchor='w', padx=10, pady=5)
+    grade_entry.pack(anchor='w', padx=10, pady=5)
     #literature entry
     lit_entry = tk.Entry(frame)
     lit_label = tk.Label(frame, text="Literature Keystone Score:", font = ("Times New Roman", 9))
@@ -113,17 +135,22 @@ def create_info_screen(container):
     bio_label.pack(anchor='w', padx=10, pady=5)
     bio_entry.pack(anchor='w', padx=10, pady=5)
     #CTE entry
-    cte_entry = tk.Entry(frame)
-    cte_label = tk.Label(frame, text="Are you in a CTE class?",  font = ("Times New Roman", 9))
-
-    cte_label.pack(anchor='w', padx=10, pady=5)
-    cte_entry.pack(anchor='w', padx=10, pady=5)
+    check_cte = tk.IntVar()
+    cte_checkbox = tk.Checkbutton(frame, text=" Enrolled in CTE program? ", variable=check_cte)
+    cte_checkbox.pack(anchor='w', padx=10, pady=5)
+    check_cert = tk.IntVar()
+    cert_checkbox = tk.Checkbutton(frame, text=" Have you earned an industry certification?  ", variable=check_cert)
+    cert_checkbox.pack(anchor='w', padx=10, pady=5)
     #Any AP entry
-    ap_entry = tk.Entry(frame)
-    ap_label = tk.Label(frame, text="Any AP course scores?",  font = ("Times New Roman", 9))
+    check_ap = tk.IntVar()
+    ap_checkbox = tk.Checkbutton(frame, text=" Have you passed any AP course exam?  ", variable=check_cert)
+    ap_checkbox.pack(anchor='w', padx=10, pady=5)
 
-    ap_label.pack(anchor='w', padx=10, pady=5)
-    ap_entry.pack(anchor='w', padx=10, pady=5)
+    #PSAT entry
+    psat_entry = tk.Entry(frame)
+    psat_label = tk.Label(frame, text="PSAT scores?",  font = ("Times New Roman", 9))
+    psat_label.pack(anchor='w', padx=10, pady=5)
+    psat_entry.pack(anchor='w', padx=10, pady=5)
     #SAT entry
     sat_entry = tk.Entry(frame)
     sat_label = tk.Label(frame, text="SAT scores?",  font = ("Times New Roman", 9))
@@ -165,6 +192,18 @@ def create_path2_screen(container):
 
     path2_title = tk.Label(frame, text="Pathway 2", font=("Times New Roman", 18), bg="#FADDC0")
     path2_title.pack(pady=50)
+
+    display_area_2 = scrolledtext.ScrolledText(frame, width=50, height=15, wrap=tk.WORD)
+    display_area_2.pack(padx=10, pady=10)
+    path2_txt = ""
+    for i in range(len(s1.result2)):
+        path2_txt += s1.result2[i] +"\n"
+        
+    long_content = path2_txt
+    display_area_2.insert(tk.INSERT, long_content)
+
+
+    
     return frame
 
 def create_path3_screen(container):
@@ -176,6 +215,15 @@ def create_path3_screen(container):
 
     path3_title = tk.Label(frame, text="Pathway 3", font=("Times New Roman", 18), bg="#FADDC0")
     path3_title.pack(pady=50)
+
+    display_area_3 = scrolledtext.ScrolledText(frame, width=50, height=15, wrap=tk.WORD)
+    display_area_3.pack(padx=10, pady=10)
+    path3_txt = ""
+    for i in range(len(s1.result3)):
+        path3_txt += s1.result3[i] +"\n"
+        
+    long_content = path3_txt
+    display_area_3.insert(tk.INSERT, long_content)
     return frame
 
 def create_path4_screen(container):
